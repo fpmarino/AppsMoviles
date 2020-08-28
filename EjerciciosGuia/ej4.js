483967467498f8a675fbae30399c031229399ad0
florencia.marinio@davinci.edu.ar <florencia.marino@davinci.edu.ar>
2020-08-28 17:47:38 -0300

    ejercicio4

---
+++ EjerciciosGuia/ej4.js
@@ -0,0 +1,13 @@
+/*Define una función que dado un array de números nos dice si alguno es par.
+
+algunoEsPar([1,2,3])
+> true*/
+
+
+
+function numeroPar(element, array) 
+{
+    return element % 2 === 0;     
+}
+  console.log([4,2,7,9,15,1,18].some(numeroPar)); // true
+  console.log([1,3,5,7,9].some(numeroPar)); // false
