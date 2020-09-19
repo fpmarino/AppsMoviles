91696486f5c50b79433c1af64773b1693dcc1ad1
florencia.marinio@davinci.edu.ar <florencia.marino@davinci.edu.ar>
2020-09-19 20:34:29 -0300

    ejercicio11

---
+++ EjerciciosGuia/ej11.js
@@ -0,0 +1,22 @@
+/*Crea una clase Persona que tenga los atributos nombre y edad. Además agregale un método presentarse() que como 
+resultado imprima un mensaje diciendo su nombre y edad.*/
+
+class Persona
+{
+    constructor(nombre, edad) 
+    {
+        this.nombre = nombre;
+        this.edad=edad;
+    }
+        
+
+    Presentarse()
+    {
+    return "Mi nombre es " + this.nombre + " y tengo " + this.edad+" años";
+    }
+
+
+}
+
+var p1 = new Persona("Florencia",23);
+console.log(p1.Presentarse());
