c806a3c195fc9ba41346a7e856f137898fb115f9
florencia.marinio@davinci.edu.ar <florencia.marino@davinci.edu.ar>
2020-09-30 15:55:08 -0300

    ej

--- ejerciciosJSAsync-master/ejercicio_6/app.js
+++
@@ -1,7 +0,0 @@
-
-
-
-const obtenerPersonaFake = require("./library");
-
-// Usar la funcion obtenerPersonaFake() la cual devuelve la promesa de traer el objeto persona extraido
-obtenerPersonaFake().then(x => console.log(x));
