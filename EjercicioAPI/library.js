8d323d32b72e99ae5d27080b74e9b55079df7a35
florencia.marinio@davinci.edu.ar <florencia.marino@davinci.edu.ar>
2020-10-01 19:24:09 -0300

    library

---
+++ EjercicioAPI/library.js
@@ -0,0 +1,11 @@
+const request = require('request-promise');
+
+
+function request (section, timeperiod){
+    var options = {
+        url: "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/{section}/{time-period}.json" + method + ".php",
+        json: true
+    }
+
+}
+
