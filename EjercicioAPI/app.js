17fd4c0896a431e68b3248ec084fbc9c766198c8
florencia.marinio@davinci.edu.ar <florencia.marino@davinci.edu.ar>
2020-10-08 12:40:39 -0300

    library

--- EjercicioAPI/app.js
+++ EjercicioAPI/app.js
@@ -3,6 +3,7 @@ const obtenerCerveceria = require ("./library");
 obtenerCerveceria().then(function (c){
     console.log("Nombre: "+c.name);
     console.log("Tipo de cerveceria: "+c.brewery_type);
+    console.log("Cale: "+c.street);
     console.log("Ciudad: "+c.city);
     console.log("Pais: "+c.country);
     console.log("Página web: "+c.website_url);
